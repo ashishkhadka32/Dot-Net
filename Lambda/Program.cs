65bec06127d4eb65c700a80999f3fce533cab96a
ashishkhadka32 <97326909+ashishkhadka32@users.noreply.github.com>
2022-12-26 20:59:19 +0545

    Add files via upload

---
+++ Lambda/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+class Student
+{
+    string Name, Location, College;
+    static void Main(string[] args)
+    {
+        List<Student> Std = new List<Student>(){
+         new Student{Name = "Ashis", Location="Kathmandu",  College ="KCT"},
+         new Student{Name = "Hikmat", Location="Kathmandu", College ="Samriddhi"},
+         new Student{Name = "Ashok", Location="Kathmandu", College="KCT"},
+    };
+       var result = Std.FindAll(x => x.Location == "Kathmandu" &&
+       x.College == "KCT");
+        foreach (var x in result)
+        {
+            Console.WriteLine($"{x.Name}\t\t {x.Location}\t\t {x.College}");
+        }
+    }
+}
