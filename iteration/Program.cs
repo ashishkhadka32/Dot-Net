372c417375614296d8541d15c6c9feb87722f797
ashishkhadka32 <97326909+ashishkhadka32@users.noreply.github.com>
2022-12-26 21:01:11 +0545

    Add files via upload

---
+++ iteration/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iteration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i <= 10; i++)
+            {
+                int mul = num * i;
+                Console.WriteLine("{0}*{1}={2}", num, i, mul);
+            }
+        }
+    }
+}
---
+++ odd/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+                Console.WriteLine("{0} is a even number", num);
+            else
+                Console.WriteLine("{1} is a od number", num);
+            Console.ReadKey();
+        }
+        
+    }
+}
