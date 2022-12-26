2d2f509e60b04ce70e58d92efdb294e0f38fe5d6
ashishkhadka32 <97326909+ashishkhadka32@users.noreply.github.com>
2022-12-26 20:57:30 +0545

    Add files via upload

---
+++ Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int fact = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine($"Factorial of {num} is {fact}");
+
+        }
+    }
+}
---
+++ list/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("ram sam");
+            list.Add("gita sita");
+
+            foreach(string item in list)
+            {
+                Console.WriteLine(item);
+              
+            }
+            Console.ReadKey();
+        }
+    }
+}
